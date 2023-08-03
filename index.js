26f61c19b4e3b283da98e6eac2124f1ce2bbe5b4
kebean <chenqiua@gmail.com>
2023-08-03 16:52:59 +0300

    A function that starts the game

--- index.js
+++ index.js
@@ -30,33 +30,33 @@ function playRound(getComputerChoice, playerSelection){
         return "You both have the same guess! try again.";
     }
 }
-// function game(){
-//     let winner = 0;
-//     let looser = 0;
-//     for (let i = 0; i < 5; i++){
-//       let result = playRound(computerSelection, playerSelection);
-//        if(result.includes("You win")){
-//            winner++;
-//            console.log(result);
-//         }else if(result.includes("You loose")){
-//             looser++;
-//             console.log(result);
-//         }else{
-//             console.log("You both have the same guess! try again.");
-//         }
-//     } 
-//     // calculating the overall winner of the game after 5 rounds.
-//     if(winner > looser){
-//         console.log("You win against the computer, Congrats!");
-//     }else if(winner < looser){
-//         console.log("Oops! You loose against the computer.")
-//     }else if(winner == 0 && looser == 0 || winner == looser){
-//         console.log("you both have the same points.");
-//         return;
-//     } 
-// }
+function game(){
+    let winner = 0;
+    let looser = 0;
+    for (let i = 0; i < 5; i++){
+      let result = playRound(computerSelection, playerSelection);
+       if(result.includes("You win")){
+           winner++;
+           console.log(result);
+        }else if(result.includes("You loose")){
+            looser++;
+            console.log(result);
+        }else{
+            console.log("You both have the same guess! try again.");
+        }
+    } 
+    // calculating the overall winner of the game after 5 rounds.
+    if(winner > looser){
+        console.log("You win against the computer, Congrats!");
+    }else if(winner < looser){
+        console.log("Oops! You loose against the computer.")
+    }else if(winner == 0 && looser == 0 || winner == looser){
+        console.log("you both have the same points.");
+        return;
+    } 
+}
 
-// // console.log(playRound(computerSelection, playerSelection));
-// game();
+// console.log(playRound(computerSelection, playerSelection));
+game();
 
 
